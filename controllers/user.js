a5418d9a4fa02f1535469ea526099436d00a7368
oseeshogun <omasuaku@gmail.com>
2022-11-22 14:53:34 +0100

    fix: send user avatar

--- controllers/user.js
+++ controllers/user.js
@@ -3,14 +3,29 @@ const UserModal = require('../models/user')
 const ObjectId = require('mongoose').Types.ObjectId
 
 const getUserByToken = (req, res) => {
-  return res.json(req.user)
+  const { id } = req.user
+  UserModal.findOne({ _id: new ObjectId(id) })
+    .then((user) => {
+      res.json({
+        username: user.username,
+        id: user._id,
+        avatar: user.avatar,
+      })
+    })
+    .catch((error) => res.status(400).json({ error }))
 }
 
 const getAllUsers = (req, res) => {
   const { id } = req.user
   UserModal.find({ _id: { $ne: new ObjectId(id) } })
     .then((users) => {
-      res.json(users.map((user) => ({ username: user.username, id: user._id })))
+      res.json(
+        users.map((user) => ({
+          username: user.username,
+          id: user._id,
+          avatar: user.avatar,
+        }))
+      )
     })
     .catch((error) => res.status(400).json({ error }))
 }
@@ -25,10 +40,11 @@ const changeProfil = async (req, res) => {
   const { id } = req.user
   const { url } = req.body
 
-  UserModal.findOneAndUpdate(
-    { _id: { $ne: new ObjectId(id) } },
-    { avatar: url }
-  ).then((user) => res.json(user))
+  UserModal.findOneAndUpdate({ _id: new ObjectId(id) }, { avatar: url })
+    .then((user) => {
+      console.log(user)
+      res.json({ ...user, avatar: url })
+    })
     .catch((error) => res.status(400).json({ error }))
 }
 
